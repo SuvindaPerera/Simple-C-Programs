6076cd409e3fbb9b81e2c1ca222b3fc4fdb9cef7
Suvinda Perera <48805257+SuvindaPerera@users.noreply.github.com>
2022-09-26 18:52:12 +0530

    User input

---
+++ Get User Input/userInput.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declare a variable to store the user input
+    int a;
+
+    // Tell the user what to do
+    printf("Enter a number: ");
+
+    // Get the user input
+    scanf("%d", &a);
+
+    // Print the user input
+    printf("What you have enterd is %d", a);
+
+    return 0;
+}
